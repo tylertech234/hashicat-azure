7bc9faf12e68225c2ee49f9ae7b4c89e3bae6056
tyler <tyler234@gmail.com>
2022-09-23 16:29:16 +0000

    correct spelling of resource

--- variables.tf
+++ variables.tf
@@ -74,7 +74,7 @@ variable "placeholder" {
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
 
-variable "resouce_group" {
+variable "resource_group" {
   default       = "meow-rg-001"
   description   = "RG name"
 }
